b2cd4c4330a4514c1be9a9b880389f89eb056578
Gizar <gizarir@gmail.com>
2023-03-16 20:30:29 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# AliceSkillDjango
+
+Steps for start process of developing:
+1. pip install -r requirements.txt 
+2. Start application: python app.py
+3. Start new terminal and start ngrok: ngrok http 8000 
+4. Move https address ngrok to option Webhook in your Alice Skill
---
+++ app/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+import json
+
+import logging
+
+from django.views.decorators.csrf import csrf_exempt
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@csrf_exempt
+def handler(request):
+    """
+    Entry-point for Django server.
+    :param request: request payload.
+    :return: response to be serialized as JsonResponse .
+    """
+    try:
+        event = json.loads(request.body.decode())
+    except ValueError:
+        return JsonResponse({
+            'error': 'Ошибка преобразования данных запроса в json',
+        })
+
+    logging.info(event)
+    logging.info(type(event))
+
+    response = {
+        "version": event["version"],
+        "session": event["session"],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    if event["session"]["new"]:
+        response["response"]["text"] = "Привет! Как твои дела? Как отметил новый год?"
+    else:
+        if event["request"]["original_utterance"].lower() in ["хорошо","отлично"]:
+            response["response"]["text"] = "Супер! Я за вас рада!"
+        elif event["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
+            response["response"]["text"] = "Жаль, думаю со мной было бы лучше!"
+
+    return JsonResponse(response)
